905eb70166f804c05b6af850a5816d97164f9a89
ashwin kumar <ashwinkumar2468@gmail.com>
2018-09-28 13:02:21 +0530

    modified filter log reducer

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,121 +1,114 @@
 // consider data logs sample
-const logs=[
-    {    
-      "id":"20801",
-      "date": "2018-09-27T00:00:00.000Z",
-    "user":{
-      "requester":{
-        "ip":"127.0.0.1",
-        "nat_type":"EIM",
-       "os":"linux"
-    },
-    "responder":{
-       "ip":"127.0.0.1",
-       "nat_type":"EIM",
-       "os":"linux"
-    }
-    },
-    "connection_types":{
-     "direct":"true",
-       "utp_hp":{
-       "Succeeded":{
-          "time_spent":{
-             "secs":0,
-             "nanos":0
-          }
-       }
-    },
-      "tcp_hp": "Failed"
+const logs = [
+  {
+  "createdAt": "2018-09-28T05:27:27.242Z",
+  "peer_requester": {
+    "ip": "182.74.140.194",
+    "nat_type": "EIM",
+    "os": "win64",
+    "geo_info": {
+      "region": "Karnataka",
+      "region_code": "KA",
+      "country": "IN",
+      "country_name": "India"
     }
   },
-  {
-      "id":"20802",
-      "date": "2018-09-22T00:00:00.000Z",
-    "user":{
-      "requester":{
-        "ip":"127.0.0.1",
-        "nat_type":"EIM",
-       "os":"linux"
-    },
-    "responder":{
-       "ip":"127.0.0.1",
-       "nat_type":"EIM",
-       "os":"linux"
-    }
-    },
-    "connection_types":{
-     "direct":"true",
-       "utp_hp":{
-       "Succeeded":{
-          "time_spent":{
-             "secs":0,
-             "nanos":0
-          }
-       }
-    },
-      "tcp_hp": "Failed"
+  "peer_responder": {
+    "ip": "84.160.11.231",
+    "nat_type": "EIM",
+    "os": "win",
+    "geo_info": {
+      "region": "Baden-Württemberg Region",
+      "region_code": "BW",
+      "country": "DE",
+      "country_name": "Germany"
     }
   },
-  {
-      "id":"20803",
-      "date": "2018-08-30T00:00:00.000Z",
-    "user":{
-      "requester":{
-        "ip":"127.0.0.1",
-        "nat_type":"EIM",
-       "os":"linux"
-    },
-    "responder":{
-       "ip":"127.0.0.1",
-       "nat_type":"EIM",
-       "os":"linux"
-    }
-    },
-    "connection_types":{
-     "direct":"true",
-       "utp_hp":{
-       "Succeeded":{
-          "time_spent":{
-             "secs":0,
-             "nanos":0
-          }
-       }
-    },
-      "tcp_hp": "Failed"
-    }
-  }
-]
-
-const filtered_logs ={
-        "id":null,
-        "date": null,
-      "user":{
-        "requester":{
-          "ip":null,
-          "nat_type":null,
-         "os":null
-      },
-      "responder":{
-         "ip":null,
-         "nat_type":null,
-         "os":null
+  "is_direct_successful": false,
+  "utp_hole_punch_result": {
+    "Succeeded": {
+      "time_spent": {
+        "secs": 0,
+        "nanos": 0
       }
-      },
-      "connection_types":{
-       "direct":null,
-         "utp_hp":{
-         "Succeeded":{
-            "time_spent":{
-               "secs":0,
-               "nanos":0
-            }
-         }
-      },
-        "tcp_hp": null
-      }    
-}  
+    }
+  },
+  "tcp_hole_punch_result": "success"
+},
+{
+  "createdAt": "2018-09-28T05:27:27.242Z",
+  "peer_requester": {
+    "ip": "182.74.140.194",
+    "nat_type": "EIM",
+    "os": "win64",
+    "geo_info": {
+      "region": "Karnataka",
+      "region_code": "KA",
+      "country": "IN",
+      "country_name": "India"
+    }
+  },
+  "peer_responder": {
+    "ip": "84.160.11.231",
+    "nat_type": "EIM",
+    "os": "win",
+    "geo_info": {
+      "region": "Baden-Württemberg Region",
+      "region_code": "BW",
+      "country": "DE",
+      "country_name": "Germany"
+    }
+  },
+  "is_direct_successful": false,
+  "utp_hole_punch_result": {
+    "Succeeded": {
+      "time_spent": {
+        "secs": 0,
+        "nanos": 0
+      }
+    }
+  },
+  "tcp_hole_punch_result": "success"
+},
+{
+  "createdAt": "2018-09-28T05:27:27.242Z",
+  "peer_requester": {
+    "ip": "182.74.140.194",
+    "nat_type": "EIM",
+    "os": "win64",
+    "geo_info": {
+      "region": "Karnataka",
+      "region_code": "KA",
+      "country": "IN",
+      "country_name": "India"
+    }
+  },
+  "peer_responder": {
+    "ip": "84.160.11.231",
+    "nat_type": "EIM",
+    "os": "win",
+    "geo_info": {
+      "region": "Baden-Württemberg Region",
+      "region_code": "BW",
+      "country": "DE",
+      "country_name": "Germany"
+    }
+  },
+  "is_direct_successful": false,
+  "utp_hole_punch_result": {
+    "Succeeded": {
+      "time_spent": {
+        "secs": 0,
+        "nanos": 0
+      }
+    }
+  },
+  "tcp_hole_punch_result": "success"
+}
+]
+const filtered_logs = [{}]
 
 export default {
-    logs,
-    filtered_logs,
+  logs,
+  filtered_logs,
 }
--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -2,7 +2,6 @@
 import { UI_ACTIONS } from '../constants/action_types';
 import data from "../assets/data"
 
-
 export default (state={}, action) => {
   console.log("state in reducer",state);
   // switch (action.type) {
@@ -16,45 +15,53 @@ export default (state={}, action) => {
 };
 
 // used for sorting may be shifted to utils
-function filterLogs(rawData,days){
-  var endDate = new Date(Date.now());
+function CalcDate(rawData,type,duration){
+  var endDate = new Date();
   var startDate = new Date();
-  startDate.setDate(startDate.getDate()-days);
-  var resultProductData = rawData.filter((product)=>{
-    var date = new Date(product.date.toString());
-    return (date >= startDate && date <= endDate);
-  });
-  return resultProductData;
+  type==="day" ? startDate.setDate(endDate.getDate()-duration) : startDate.setHours(endDate.getHours()-duration);
+  return filterLogs(rawData,startDate,endDate)
   }
+function filterLogs(rawData,startDate,endDate){
+var resultProductData = rawData.filter((product)=>{
+  var date = new Date(product.createdAt.toString());
+  return (date >= startDate && date <= endDate);
+});
+return resultProductData;
+}  
   
 export const filterLogReducer = (state={}, action)=>{
   var filteredData;
   switch (action.type){
     case "filterMonth":{
-      filteredData = filterLogs(state.logs,30);
+      // console.log("in switch",state.logs);
+      filteredData = CalcDate(state.logs,"day",30);
       return state = {...state, filtered_logs:filteredData }  
     }
 
     case "filterWeek":{
-      filteredData = filterLogs(state.logs,7);
+      filteredData = CalcDate(state.logs,"day",7);
       return state = {...state, filtered_logs:filteredData }  
     }
 
     case "filterDay":{
-      filteredData = filterLogs(state.logs,1);
+      filteredData = CalcDate(state.logs,"day",2);
       return state = {...state, filtered_logs:filteredData }   
     }
 
     case "filterHour":{
-    filteredData = filterLogs(state.logs,action.payload); //check logic for 1h
-    return state = {...state, filtered_logs:filteredData }  //update parameter
+    filteredData = CalcDate(state.logs,"hour",1); 
+    return state = {...state, filtered_logs:filteredData }  
   }
-  //setting initial state via reducer
+  case "filterCustom":{
+    filteredData = filterLogs(state.logs,new Date(action.startDate),new Date(action.endDate)); 
+    return state = {...state, filtered_logs:filteredData }  
+  }
   default: {return state=data}
 }
 } 
-
 // to call dispatcher 
-//dispatch({type:"filterMonth"});
-//dispatch({type:"filterWeek"});
-//dispatch({type:"filterDay"});
+// dispatch({type:"filterMonth"});
+// dispatch({type:"filterWeek"});
+// dispatch({type:"filterDay"});
+// dispatch({type:"filterHour"});
+// dispatch({type:"filterCustom",startDate:"2018-08-28",endDate:"2018-09-28"});
